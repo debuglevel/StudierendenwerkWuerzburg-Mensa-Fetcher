bfadbaef2baa6573c6809c7b0a01856245656ef5
Marc Kohaupt <debuglevel@gmail.com>
2025-07-18 13:19:01 +0200

    feat: extract day menus

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import requests
+import bs4
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+sites = [
+    "https://www.swerk-wue.de/bamberg/essen-trinken/mensen-speiseplaene/mensa-austrasse-bamberg/menu",
+    "https://www.swerk-wue.de/bamberg/essen-trinken/mensen-speiseplaene/mensa-feldkirchenstrasse-bamberg/menu",
+]
 
+import logging
+from logfmter import Logfmter
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+handler = logging.StreamHandler()
+handler.setFormatter(Logfmter())
+logging.basicConfig(handlers=[handler], level=logging.DEBUG)
 
+logger = logging.getLogger("mensa_scraper")
+
+def get_site_content(url):
+    logger.debug("Fetching site content...", extra={"url": url})
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an error for bad responses
+    logger.debug("Fetched site content.", extra={"url": url, "status_code": response.status_code, "length": len(response.text)})
+    return response.text
+
+def get_day_menus_html(content):
+    logger.debug("Getting day menus HTML...")
+    soup = bs4.BeautifulSoup(content, "html.parser")
+    day_menus = [str(div) for div in soup.find_all("div", class_="day-menu")]
+    logger.debug(f"Got day menus HTML.", extra={"count": len(day_menus)})
+    return day_menus
+
+def main():
+    for site in sites:
+        content = get_site_content(site)
+        day_menus_html = get_day_menus_html(content)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
